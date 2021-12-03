493867024e7a4103882abb35bcadb1fce83623f0
BlackPrince163 <4523120100Ni>
2021-12-04 00:30:32 +0300

    + server class

--- tanks_server/src/main/java/com/example/tanks_server/Server.java
+++ tanks_server/src/main/java/com/example/tanks_server/Server.java
@@ -1,4 +1,31 @@
 package com.example.tanks_server;
 
+import com.example.tanks_server.connection.Connection;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+    private volatile List<Connection> connections;
+    private static int id = 0;
+
+    public Server() throws IOException {
+        connections = new ArrayList<>();
+        init();
+    }
+
+    private void init() throws IOException {
+        ServerSocket s1 = new ServerSocket(Protocol.PORT);
+        while (true) {
+            Socket client = s1.accept();
+            connections.add(new Connection(this, client, id++));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+    }
 }
