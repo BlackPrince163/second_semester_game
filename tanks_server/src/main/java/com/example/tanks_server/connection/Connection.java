87214a105f623667b1da558fa59a8b790d74ee3b
BlackPrince163 <4523120100Ni>
2021-12-04 00:30:47 +0300

    + connection class in server

--- tanks_server/src/main/java/com/example/tanks_server/connection/Connection.java
+++ tanks_server/src/main/java/com/example/tanks_server/connection/Connection.java
@@ -1,4 +1,26 @@
 package com.example.tanks_server.connection;
 
-public class Connection {
+import com.example.tanks_server.Server;
+
+import java.net.Socket;
+
+public class Connection implements Runnable{
+    private Socket socket;
+    private Thread thread;
+    private Server server;
+    private int id;
+
+
+    public Connection(Socket socket, Thread thread, Server server, int id) {
+        this.server = server;
+        this.socket = socket;
+        this.id = id;
+        this.thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
