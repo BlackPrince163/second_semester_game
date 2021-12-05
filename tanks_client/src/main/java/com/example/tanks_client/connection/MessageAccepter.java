eff9e5744c0bded7f87c9877210719b1b6eeb2bc
BlackPrince163 <4523120100Ni>
2021-12-05 14:58:51 +0300

    + connection

---
+++ tanks_client/src/main/java/com/example/tanks_client/connection/MessageAccepter.java
@@ -0,0 +1,14 @@
+package com.example.tanks_client.connection;
+
+import com.example.tanks_client.SocketClient;
+
+public class MessageAccepter implements Runnable {
+    private SocketClient socket;
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
