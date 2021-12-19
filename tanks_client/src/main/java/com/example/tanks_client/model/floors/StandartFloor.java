595ab4150a6a6aefc28e6d60635387fa10a56c5b
BlackPrince163 <4523120100Ni>
2021-12-19 21:56:14 +0300

    + models floor

--- tanks_client/src/main/java/com/example/tanks_client/model/floors/StandartFloor.java
+++ tanks_client/src/main/java/com/example/tanks_client/model/floors/StandartFloor.java
@@ -3,10 +3,9 @@ package com.example.tanks_client.model.floors;
 import com.example.tanks_client.Main;
 
 public class StandartFloor extends Floor {
-    // тупо черный пол
     private static final String floorPath = Main.class.getResource("/img/standartFloor.png").toString();
 
-    public StandartFloor(String path, int width, int height, int col, int row) {
-        super(path, width, height, col, row);
+    public StandartFloor(int width, int height, int col, int row) {
+        super(floorPath, width, height, col, row);
     }
 }
