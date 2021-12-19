8877a7c64520bebcedc2e5df5d23631fdf0f1dbb
BlackPrince163 <4523120100Ni>
2021-12-19 21:53:00 +0300

    change connection

--- tanks_server/src/main/java/com/example/tanks_server/connection/Connection.java
+++ tanks_server/src/main/java/com/example/tanks_server/connection/Connection.java
@@ -1,26 +1,65 @@
 package com.example.tanks_server.connection;
 
+import com.example.tanks_protocol.Message;
 import com.example.tanks_server.Server;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Iterator;
 
-public class Connection implements Runnable{
+public class Connection implements Runnable {
     private Socket socket;
     private Thread thread;
     private Server server;
     private int id;
 
-
-    public Connection(Socket socket, Thread thread, Server server, int id) {
+    public Connection(Server server, Socket socket, int id) {
         this.server = server;
         this.socket = socket;
         this.id = id;
-        this.thread = new Thread(this);
+        thread = new Thread(this);
         thread.start();
     }
 
-    @Override
     public void run() {
+        try{
+            while (true) {
+                // Получение данных от клиента.
+                Message response = Message.readMessage(socket.getInputStream());
 
+                // Ответ клиенту.
+                Iterator<Connection> iterator = server.connectionsIterator();
+                while(iterator.hasNext()){
+                    Connection connection = iterator.next();
+                    if (!connection.equals(this)) {
+                        sendMessage(response, connection.getSocket());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            try {
+                if (e instanceof SocketTimeoutException) {
+                    throw new SocketTimeoutException();
+                } else {
+                    e.printStackTrace();
+                }
+            } catch (SocketTimeoutException ste) {
+                System.out.println("Turn off the server by timeout");
+            }
+        }
+    }
+
+    public void sendMessage(Message message, Socket socket) {
+        try {
+            socket.getOutputStream().write(Message.getBytes(message));
+            socket.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
     }
 }
