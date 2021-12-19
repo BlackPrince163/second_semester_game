53dc654d73976140ff26f911aea3e3554ad84ff9
BlackPrince163 <4523120100Ni>
2021-12-19 21:57:55 +0300

    ! Tried changing, doesn't work (

--- tanks_server/src/main/java/module-info.java
+++ tanks_server/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tanks_server {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.example.tanks_protocol;
 
 
     opens com.example.tanks_server to javafx.fxml;
