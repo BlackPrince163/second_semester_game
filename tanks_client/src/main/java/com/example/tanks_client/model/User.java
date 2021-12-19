db76db414ba74d783ba7fee142dd1745d270f008
BlackPrince163 <4523120100Ni>
2021-12-19 21:55:03 +0300

    + new features to user

--- tanks_client/src/main/java/com/example/tanks_client/model/User.java
+++ tanks_client/src/main/java/com/example/tanks_client/model/User.java
@@ -1,7 +1,49 @@
 package com.example.tanks_client.model;
 
+import com.example.tanks_client.model.Tank.PixelTank;
+import com.example.tanks_client.model.Tank.Tank;
+import com.example.tanks_protocol.UserAction;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
+    protected Tank tank;
+    protected volatile UserAction action;
+    private volatile Boolean flag;
+    private static int id = 0;
+    private int user_id;
 
+    public User(){
+        this.user_id = id;
+        this.flag = false;
+        this.tank = new PixelTank(30, 30, 6, 6);
+    }
+
+    public User(Tank tank, UserAction action, Boolean flag) {
+        this.tank = tank;
+        this.action = action;
+        this.flag = flag;
+    }
+
+    public void setAction(UserAction action){
+        flag = true;
+        this.action = action;
+    }
+
+    public UserAction getAction(){
+        this.flag = false;
+        return this.action;
+    }
+
+    public Boolean getFlag(){
+        return flag;
+    }
+
+    public Tank getTank(){
+        return this.tank;
+    }
+
+    public int getUserId() {
+        return user_id;
+    }
 }
