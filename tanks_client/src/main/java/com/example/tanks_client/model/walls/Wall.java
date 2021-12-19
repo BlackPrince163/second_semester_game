c3bdace615eced8840b22d233081e9272a7be534
BlackPrince163 <4523120100Ni>
2021-12-19 21:55:21 +0300

    + model walls

---
+++ tanks_client/src/main/java/com/example/tanks_client/model/walls/Wall.java
@@ -0,0 +1,24 @@
+package com.example.tanks_client.model.walls;
+
+import com.example.tanks_client.model.GameElements;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public abstract class Wall extends GameElements {
+    protected int col;
+    protected int row;
+
+    public Wall(String path, int width, int height, int col, int row) {
+        super(path, width, height);
+        this.col = col;
+        this.row = row;
+    }
+
+    public void makeImage(GridPane lawn){
+        img = new ImageView();
+        Image im = new Image(path, width, height,false,false);
+        img.setImage(im);
+        lawn.add(img, col, row,1,1);
+    }
+}
