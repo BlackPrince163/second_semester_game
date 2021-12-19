86f1ef5627cafd6dea3e81608fe146c1a05b4f15
BlackPrince163 <4523120100Ni>
2021-12-19 21:54:23 +0300

    fix bug in controller

--- tanks_client/src/main/java/com/example/tanks_client/controllers/Menu.java
+++ tanks_client/src/main/java/com/example/tanks_client/controllers/Menu.java
@@ -1,4 +1,29 @@
 package com.example.tanks_client.controllers;
 
+import com.example.tanks_client.model.User;
+import com.example.tanks_protocol.UserAction;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Menu {
+    User user;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private void exit(MouseEvent e){
+        user.setAction(UserAction.EXIT);
+        System.exit(0);
+    }
+    public void setUser(User user){
+        this.user = user;
+    }
+
 }
